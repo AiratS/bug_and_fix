d7a1468f7a589b8cde6985a70ada9ffe3e4dec1e
Сиразов Айрат <sirazov@infomatika.ru>
2025-12-04 16:16:55 +0300

    feat: добваить миграцию для создания таблицы bugs

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/airats/bug_and_fix/internal/app"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	app.Run()
+	urlExample := "postgres://user:password@localhost:5432/bug_and_fix?sslmode=disable"
+	conn, err := pgx.Connect(context.Background(), urlExample)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	// app.Run()
 }
---
+++ migrations/20251204123756_create_bugs_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE bugs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(1024) NOT NULL,
+    description TEXT NOT NULL,
+    project VARCHAR(255) NOT NULL,
+    error_message TEXT,
+    device VARCHAR(127) NOT NULL,
+    env INTEGER NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table bugs;
+-- +goose StatementEnd
